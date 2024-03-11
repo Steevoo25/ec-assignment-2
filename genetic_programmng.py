9cf64fe25da70012b286a4053d8a7b1698858f80
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-11 21:18:20 +0000

    rename to tree_depth

--- genetic_programmng.py
+++ genetic_programmng.py
@@ -1,4 +1,4 @@
-import time
+import signal # timer
 import random
 import sexpdata as sex
 from treelib import Node, Tree
@@ -6,6 +6,7 @@ from s_expressions import evaluate
 
 function_nodes = [('add', 2),('sub', 2),('mul', 2),('div', 2),('pow', 2),('sqrt', 1),('log', 1),('exp', 1),('max', 2),('ifleq', 4),('data', 1),('diff', 2),('avg', 2)]
 function_nodes = [('add', 2),('sub', 2),('mul', 2),('div', 2),('pow', 2),('sqrt', 1),('log', 1),('exp', 1),('max', 2),('ifleq', 4)] #,('data', 1),('diff', 2),('avg', 2)]
+#function_nodes = [('add', 2),('sub', 2),('mul', 2),('div', 2),('sqrt', 1),('log', 1),('max', 2),('ifleq', 4)] #,('data', 1),('diff', 2),('avg', 2)]
 
 
 SAMPLE_EXP_1 = "(mul (add 1 2) (log 8))"
@@ -14,6 +15,7 @@ SAMPLE_EXP_2 = "(max (data 0) (data 1))"
 # Branch swap - swap 2 branches from parents
 def crossover(parent1, parent2, branch_index: int):
     return 1
+
 # Branch replacement - Pick a random branch, replace with newly generated branch
 def mutation(parent):
     mutated = parent.copy()
@@ -29,21 +31,21 @@ def tournament_selection(population: list, fitnesses: list, n: int, offspring_si
     return 1
 
 # Generates a single member of population using full generation
-def full_generation(depth) -> str:
-    if depth == 0:
+def full_generation(tree_depth) -> str:
+    if tree_depth == 0:
         # At the leaf level, generate a random function or data node
         return f"{random.randint(1, 10)}"
     else:
         # Choose a random function node with its children
         node_name, num_children = random.choice(function_nodes)
-        children = ' '.join(full_generation(depth - 1) for _ in range(num_children))
+        children = ' '.join(full_generation(tree_depth - 1) for _ in range(num_children))
         return f"({node_name} {children})"
 
 # Generates a population of given size - Ramped half and half
-def generate_population(population_size: int, max_depth: int) -> list:
+def generate_population(population_size: int, tree_depth: int) -> list:
     population = []
     for _ in range(population_size):
-        population.append(full_generation(max_depth))
+        population.append(full_generation(tree_depth))
     return population
 
 def calculate_fitness(solution):
@@ -55,10 +57,14 @@ def calclulate_fitnesses(population: list):
         fitnesses.append(calculate_fitness(solution))
     return fitnesses
 
-def ga(population_size: int, time_budget: int):
+def ga(population_size: int, time_budget: int, tree_depth: int ):
     time_elapsed = 0
-    population = generate_population(population_size, 2)
-
+    population = generate_population(population_size, tree_depth)
+    
+    # for solution in population:
+    #     print(solution)
+    #     print(evaluate(sex.loads(solution)))
+        
     fitnesses = calclulate_fitnesses(population)
     
     while time_elapsed > time_budget:
@@ -69,4 +75,4 @@ def ga(population_size: int, time_budget: int):
         time_elapsed +=1
     return 1
 
-ga(3,2)
+ga(7,2,3)
