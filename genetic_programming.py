9d57bc2787ca7dede6084d360373e02dc4952775
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-11 14:37:15 +0000

    started on evaluation

--- genetic_programming.py
+++ genetic_programming.py
@@ -4,8 +4,20 @@ import math
 SAMPLE_EXP_1 = "(mul (add 1 2) (log 8))"
 SAMPLE_EXP_2 = "(max (data 0) (data 1))"
 
+ADD = 'add'
+LOG = 'log'
+MUL = 'mul'
+
 def evaluate_exp(exp):
-    print("Evaluating", exp)
+    arity = len(exp)
+    operator = sex.car(exp)
+    operands = sex.cdr(exp)
+    print("Evaluating", exp,"Arity", arity,  "Operator", operator, "Operands", operands)
+    
+    if operator == ADD:
+        print("Adding", operands)
+        return add(*operands)
+
 # adds 2 expressions
 def add(exp1, exp2):
     return exp1 + exp2
@@ -21,13 +33,12 @@ expression_2 = sex.loads(SAMPLE_EXP_2)
 # get Operand(s) = cdr()
 
 def splitExpression(exp):
-    
+    print(sex.car(exp))
     try:
-        print(sex.car(exp))
-        evaluate_exp(exp)
         splitExpression(sex.cdr(exp))
     except IndexError: # Atoms have been reached
         print("End of expression reached")
+    evaluate_exp(sex.car(exp)) 
     return 
 
 splitExpression(expression_1)
