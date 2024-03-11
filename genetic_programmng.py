eff4fe39db9eeb8b7811b4bd21d6bbf4e99ec61c
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-11 19:12:02 +0000

    more skelly code

--- genetic_programmng.py
+++ genetic_programmng.py
@@ -1,20 +1,66 @@
+import time
+import random
+import sexpdata as sex
 
-# Branch swap
-def crossover():
+SAMPLE_EXP_1 = "(mul (add 1 2) (log 8))"
+SAMPLE_EXP_2 = "(max (data 0) (data 1))"
+
+#function_nodes = [('add', 2),('sub', 2),('mul', 2),('div', 2),('pow', 2),('sqrt', 1),('log', 1),('exp', 1),('max', 2),('ifleq', 4),('data', 1),('diff', 2),('avg', 2)]
+
+# Branch swap - swap 2 branches from parents
+def crossover(parent1, parent2, branch_index):
+    return 1
+# Branch replacement - Pick a random branch, replace with newly generated branch
+def mutation(parent):
+    mutated = parent.copy()
+    # select random branch and delete it
+    # generate new branch and add it where branch was deleted from
+    return 
+
+# Generates a single member of population using full generation
+def full_generation(tree_depth):
+    
     return 1
 
-def mutation():
+# Generates a single member of population using growth generation
+def growth_generation(max_depth):
+    
     return 1
 
-# Generates a population of given size
-def generate_population(population_size):
+def tournament_selection(population, fitnesses, n, offspring_size):
+    return 1
+    
+
+# Generates a population of given size - Ramped half and half
+def generate_population(population_size, max_depth):
     population = []
     for _ in range(population_size):
-        population.append(generate_solution())
+        #population.append(generate_solution(max_depth))
+        population = population
     return population
 
-def calculate_fitness():
+def calculate_fitness(solution):
     return 1
+    
+def calclulate_fitnesses(population):
+    fitnesses = []
+    for solution in population:
+        fitnesses.append(calculate_fitness(solution))
+    return fitnesses
 
 def ga(population_size: int, time_budget: int):
+    time_elapsed = 0
+    population = generate_population(population_size)
+    fitnesses = calclulate_fitnesses(population)
+    
+    while time_elapsed > time_budget:
+        
+        # Selection
+        # Variation
+        # Fitness Calculation
+        # Reproduction
+        time_elapsed +=1
     return 1
+    
+sexp = sex.loads(SAMPLE_EXP_1)
+print(sexp)
