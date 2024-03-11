c184e38c4cba22809df806039f93fe3db4b3c3ca
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-11 18:25:11 +0000

    skeleton code outline

--- genetic_programmng.py
+++ genetic_programmng.py
@@ -1,2 +1,20 @@
-def ga():
+
+# Branch swap
+def crossover():
+    return 1
+
+def mutation():
+    return 1
+
+# Generates a population of given size
+def generate_population(population_size):
+    population = []
+    for _ in range(population_size):
+        population.append(generate_solution())
+    return population
+
+def calculate_fitness():
+    return 1
+
+def ga(population_size: int, time_budget: int):
     return 1
