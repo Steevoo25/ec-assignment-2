efb45886d5d1e812c0fae06bcbfd66730176355c
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-12 17:03:11 +0000

    added code for parameter tuning

--- ga_tuning.py
+++ ga_tuning.py
@@ -1,151 +1,46 @@
 import random
 import time
-from s_expressions import calculate_fitness, evaluate
+from s_expressions import ga
 import optuna
-FUNCTION_NODES = [('add', 2),('sub', 2),('mul', 2),('div', 2),('pow', 2),('sqrt', 1),('log', 1),('exp', 1),('max', 2),('ifleq', 4),('data', 1),('diff', 2),('avg', 2)]
+import sexpdata as sex
+import pandas as pd
+from matplotlib import pyplot as plt
 
-# Branch swap - swap 2 branches from parents
-def branch_swap_crossover(parent1: str, parent2: str, tree_depth: int, min_depth: int):
-    branch_depth = random.randint(min_depth, tree_depth) # from 1 to avoid replacing whole tree
-    branch = get_branch_at_depth(parent2, branch_depth) # select 2 random branches
-    #swap branches
 
-    try:
-        parent1 = parent1.replace(get_branch_at_depth(parent1, branch_depth), branch, 1) 
-    except AttributeError:
-        print("wrongAttribute in crossover")
-    return parent1
+if __name__ == "__main__":
+    # Test different parameter setting and collect the data (plot)
+    columns = []
 
-# Performs the branch swap crossover for a list of parents
-def crossover(parents: list, tree_depth: int, min_depth: int, offspring_size: int):
-    offspring = []
-    # pick 2 parents
-    for _ in range(offspring_size):
-        parent1 = parents[random.randint(0,offspring_size-1)]
-        parent2 = parents[random.randint(0,offspring_size-1)]
-        offspring.append(branch_swap_crossover(parent1, parent2, tree_depth, min_depth))
+    # Initialise df columns
+    df = pd.DataFrame(columns=columns)
 
-    return offspring
+    # Returns fitness of SA with randomly generated parameters
+    def objective(trial):
+        # Define parameter ranges
 
-# Branch replacement - Pick a random branch, replace with newly generated branch of same depth
-def branch_replacement_mutation(parent: str, treedepth: int) -> str:
-    branch_depth = random.randint(1, treedepth-1)
-    branch = get_branch_at_depth(parent, branch_depth)
-    replacement = full_generation(treedepth - branch_depth)
-    new =  parent.replace(branch, replacement , 1)
-    return new
+        # Perform SA
+        _, fitness = ga()
+        # Store results in df
+        df.loc[trial.number] = ""
+        return fitness
 
-# Performs mutation on the parents with a given probability
-def mutation(parents: list, treedepth: int, mutation_rate: float) -> list:
-    for parent in parents:
-        if random.uniform(0,1) < mutation_rate:
-            parent = branch_replacement_mutation(parent, treedepth)
-            try:
-                if ')(' in parent:
-                    parent = parent.replace(')(', ') (')
-            except TypeError:
-                print("fortnite")
-    return parents
-    
-# Returns a random branch at a given depth
-def get_branch_at_depth(exp: str, depth: int) -> str:
-    # remove leading and trailing ()
-    exp = exp[1:-1]
-    temp_exp = exp
-    current_depth = 0
-    for i, char in enumerate(exp):
-        if current_depth == depth:
-            return temp_exp
-            
-        else:
-            if char == '(':
-                current_depth +=1
-                temp_exp = find_balanced_expression(exp[i:])
-                
-    return temp_exp
+    # Tune parameters
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=100)
 
-# Returns a list of all sub-expressions at the uppermost level    
-def find_balanced_expression(exp: str) -> str:
-    # monitor number of l and r brackets
-    l_brac, r_brac = 0, 0
-    start = 0
-    expressions = []
-    
-    for i, char in enumerate(exp):
-        if char == '(':
-            l_brac +=1
-            
-        if char == ')':
-            r_brac +=1
-            
-        if r_brac > l_brac: # if a close has been read before an open, ignore it
-            r_brac = 0
-            
-        if l_brac == r_brac and l_brac > 0: # If brackets are present and balanced 
-            expressions.append(exp[start:i+1].strip()) # add it to list, removing whitespace
-            start = i + 2 # check next part of list
-            l_brac, r_brac = 0,0 # reset bracket counters
-    if expressions == []:
-        print("No expressions found")
-    return random.choice(expressions)
+    # Print best results
+    print(study.best_params)
 
-# Performs tournamnt selection on a population
-def tournament_selection(population: list, n: int, offspring_size: int, population_size: int) -> list:
+    # Show whole dataframe
+    pd.set_option('display.max_rows', None)
 
-    offspring = []
-    for i in range(offspring_size):
-        tournament = []
-        for j in range(n):
-            tournament.append(population[random.randint(0,population_size-1)])
-        tournament = sorted(tournament, key=lambda x: calculate_fitness(x))
-        # parent with lowest mse
-        offspring.append(tournament[0])
-    
-    return offspring
+    # Show all results
+    df = df.sort_values(by='fitness')
+    df.index.name = 'Index'
+    print(df)
 
-# Generates a single member of population using full generation
-def full_generation(tree_depth: int) -> str:
-    if tree_depth == 0:
-        # At the leaf level, generate a random function or data node
-        return f"{random.randint(1, 10)}"
-    else:
-        # Choose a random function node with its children
-        node_name, num_children = random.choice(FUNCTION_NODES)
-        children = ' '.join(full_generation(tree_depth - 1) for _ in range(num_children))
-        return f"({node_name} {children})"
+    # print mean and standard deviations
+    print(df.mean())
+    print(df.std())
 
-# Generates a population of given size - Ramped half and half
-def generate_population(population_size: int, tree_depth: int) -> list:
-    population = []
-    for _ in range(population_size):
-        population.append(full_generation(tree_depth))
-    return population
-
-# Replaces less fit individuals in the current population with the offspring
-def reproduction(population: list, offspring: list, offspring_size: int):
-    population = sorted(population, key=lambda x : calculate_fitness(x))
-    population[-offspring_size:] = offspring
-    return population
-
-def ga(params: list, inputs:list):
-    tree_depth, crossover_n, offspring_size, mutation_rate = params
-    
-    start_time = time.time()
-    elapsed_time = 0
-    population_size, n, m, training_data, time_budget = inputs
-    population = generate_population(population_size, tree_depth)
-    
-    while elapsed_time < time_budget:
-        # Selection
-        parents = tournament_selection(population, crossover_n, offspring_size, population_size)
-        # Variation
-        parents = mutation(parents, tree_depth, mutation_rate)
-        
-        offspring = crossover(parents, tree_depth, 2, offspring_size)
-            # Fitness Calculation
-            # Fitnesses are not maintained, but calculated when required
-        # Reproduction
-        population = reproduction(population, offspring, offspring_size)
-        elapsed_time = time.time() - start_time
-
-    return population[0]
+    #observe results
