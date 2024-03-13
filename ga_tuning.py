996ccac4cf14043a9bd451f6888dc451dde5fb9b
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-13 17:44:20 +0000

    setup tuning

--- ga_tuning.py
+++ ga_tuning.py
@@ -1,27 +1,32 @@
-import random
-import time
 from hjs115 import ga
 import optuna
-import sexpdata as sex
 import pandas as pd
 from matplotlib import pyplot as plt
 
 
 if __name__ == "__main__":
     # Test different parameter setting and collect the data (plot)
-    columns = []
+    columns = ["solution", "fitness", "tree_depth", "tournament_n", "offspring_size", "mutation_rate"]
 
     # Initialise df columns
     df = pd.DataFrame(columns=columns)
 
     # Returns fitness of SA with randomly generated parameters
     def objective(trial):
+        tree_depth = 0
+        tournament_n = 0
+        offspring_size = 0
+        mutation_rate = 0
+        params = [tree_depth, tournament_n, offspring_size, mutation_rate]
+        pop_size = 0
+        budget = 0
+        inputs = [pop_size, n, m, training_x, training_y, budget]
         # Define parameter ranges
         #time budget range = 20-60s
         # Perform SA
-        _, fitness = ga()
+        sol, fitness = ga(params=params, inputs=inputs)
         # Store results in df
-        df.loc[trial.number] = ""
+        df.loc[trial.number] = (sol, fitness, *params)
         return fitness
 
     # Tune parameters
@@ -42,5 +47,7 @@ if __name__ == "__main__":
     # print mean and standard deviations
     print(df.mean())
     print(df.std())
-
+    
+    # Generate box plots
+    
     #observe results
