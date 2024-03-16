2517b828db934a443d51a765fa09f86964da04d2
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-16 12:46:41 +0000

    tree_depth tuning

--- ga_tuning.py
+++ ga_tuning.py
@@ -19,39 +19,29 @@ if __name__ == "__main__":
 
     # Returns fitness of SA with randomly generated parameters
     def objective(trial):
+        pop_size = 100
+        time_budget = 40
         # inputs
         training_x, training_y = open_training_data(DATA_PATH)
-        pop_size = trial.suggest_int('pop_size', 20, 200)
-        time_budget = trial.suggest_int('time_budget', 20, 60)
+        # pop_size = trial.suggest_int('pop_size', 20, 200)
+        # time_budget = trial.suggest_int('time_budget', 20, 60)
         inputs = [pop_size, N, M, training_x, training_y, time_budget]
         # params
-        params = SAMPLE_GA_PARAMS
-        tree_depth = trial.suggest_int('tree_depth', 4,6)
-        tournament_n = trial.suggest_int('tournament_n', 2,10)
-        offspring_size = trial.suggest_int('offspring_size', 2, 20)
-        mutation_rate = trial.suggest_float('mutation_rate', 1, 20) / pop_size
-        penalty_weight = trial.suggest_float('penalty_weight', 1, 20)
+        tree_depth, tournament_n, offspring_size, mutation_rate, penalty_weight = SAMPLE_GA_PARAMS
+        tree_depth = trial.suggest_int('tree_depth', 3,10)
+        # tournament_n = trial.suggest_int('tournament_n', 2,10)
+        # offspring_size = trial.suggest_int('offspring_size', 2, 20)
+        # mutation_rate = trial.suggest_float('mutation_rate', 1, 20) / pop_size
+        # penalty_weight = trial.suggest_float('penalty_weight', 1, 20)
         params = tree_depth, tournament_n, offspring_size, mutation_rate, penalty_weight
-        # Define parameter ranges
-        #time budget range = 20-60s
-        # Perform SA 100 times
-        best_sol=  ''
-        best_fitness = 0
-        
-        # for i in range(10):
-        #     print(f"ga {i}th iteration")
-            #print(*params, pop_size, time_budget)
+
         sol, fitness = ga(params=params, inputs=inputs)
-            #if fitness > best_fitness:
-             #   best_sol = sol
-              #  best_fitness = fitness
-        
         # Store results in df
         df.loc[trial.number] = (sol,fitness, *params, pop_size, time_budget)
         return fitness
 
     # Tune parameters
-    study = optuna.create_study()
+    study = optuna.create_study(study_name='tree_depth')
     study.optimize(objective, n_trials=100)
 
     # Print best results
