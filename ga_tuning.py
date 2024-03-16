ae9e24ae1ee2c9c76afbd013c9da7b05a5326bce
Harry Stevenson <hjs115@student.bham.ac.uk>
2024-03-16 11:51:04 +0000

    tuning operations

--- ga_tuning.py
+++ ga_tuning.py
@@ -3,6 +3,7 @@ import optuna
 import pandas as pd
 from matplotlib import pyplot as plt
 
+SAMPLE_GA_PARAMS = [4, 2, 2, 0.1, 4] #tree_depth, tournament_n, offspring_size, mutation_rate, penalty_weight
 DATA_PATH = './data/cetdl1772small.dat'
 N = 13
 M = 999
@@ -11,29 +12,32 @@ M = 999
 if __name__ == "__main__":
     
     # Test different parameter setting and collect the data (plot)
-    columns = ["solution", "fitness", "tree_depth", "tournament_n", "offspring_size", "mutation_rate", "penalty_weight", "population_size"]
-    
-    training_x, training_y = open_training_data(DATA_PATH)
+    columns = ["solution", "fitness", "tree_depth", "tournament_n", "offspring_size", "mutation_rate", "penalty_weight", "population_size", "time_budget"]
     
     # Initialise df columns
     df = pd.DataFrame(columns=columns)
 
     # Returns fitness of SA with randomly generated parameters
     def objective(trial):
-        tree_depth = 0
-        tournament_n = 0
-        offspring_size = 0
-        mutation_rate = 0
-        params = [tree_depth, tournament_n, offspring_size, mutation_rate]
-        pop_size = 0
-        budget = 0
-        inputs = [pop_size, N, M, training_x, training_y, budget]
+        # inputs
+        training_x, training_y = open_training_data(DATA_PATH)
+        pop_size = trial.suggest_int('pop_size', 20, 400)
+        time_budget = trial.suggest_int('time_budget', 20, 60)
+        inputs = [pop_size, N, M, training_x, training_y, time_budget]
+        # params
+        params = SAMPLE_GA_PARAMS
+        tree_depth = trial.suggest_int('tree_depth', 4,20)
+        tournament_n = trial.suggest_int('tournament_n', 2,10)
+        offspring_size = trial.suggest_int('offspring_size', 2, 20)
+        mutation_rate = trial.suggest_float('mutation_rate', 1, 20) / pop_size
+        penalty_weight = trial.suggest_float('penalty_weight', 1, 20)
+        params = [tree_depth, tournament_n, offspring_size, mutation_rate, penalty_weight]
         # Define parameter ranges
         #time budget range = 20-60s
         # Perform SA
         sol, fitness = ga(params=params, inputs=inputs)
         # Store results in df
-        df.loc[trial.number] = (sol, fitness, *params)
+        df.loc[trial.number] = (sol, fitness, *params, pop_size, time_budget)
         return fitness
 
     # Tune parameters
